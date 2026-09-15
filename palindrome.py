c457172bbe7624ef99002b7f3d9a197c411646ff
coyedele <oyedeleclinton@gmail.com>
2026-09-15 10:32:25 +0100

    clean palindrome

--- palindrome.py
+++ palindrome.py
@@ -10,11 +10,11 @@ def palindrome():
         first = 0
         last = length-1
 
-        while first < last:
-            if word[first] != word[last]:
-                return f"{word} is not a palindrome"
-            first += 1
-            last -= 1
+    while first < last:
+        if word[first] != word[last]:
+            return f"{word} is not a palindrome"
+        first += 1
+        last -= 1
 
-        return f"{word} is a palindrome"
+    return f"{word} is a palindrome"
 print(palindrome())
