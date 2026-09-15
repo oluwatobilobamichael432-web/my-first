c4bad9195292f830f20f410b8379a81d85a1cc65
coyedele <oyedeleclinton@gmail.com>
2026-09-15 10:14:44 +0100

    added try execpt

--- calculator.py
+++ calculator.py
@@ -1,12 +1,11 @@
 def calculator():
 
-    num = input("Enter first number")
-    operator = input("Enter an operator")
-    ber = input("Enter second number")
-
-    first = int(num)
-    second = int(ber)
-
+    try:
+        first = int(input("Enter first number\n"))
+        operator = input("Enter an operator\n")
+        second = int(input("Enter second number\n"))
+    except:
+        return "Invalid input"
 
     if operator == "+":
         return first + second
