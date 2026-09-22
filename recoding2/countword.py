c99809a37a46454c51886055131da41db1b9e738
coyedele <oyedeleclinton@gmail.com>
2026-09-22 11:06:28 +0100

    completed

--- recoding2/countword.py
+++ recoding2/countword.py
@@ -1,10 +1,16 @@
 def count_word():
-    sentence = input("Enter a complete sentence\n")
-    count = 0
-    sentence.split()
+    sentence = input("Enter a complete sentence\n").lower().split()
+    dictionary = {}
+
     for word in sentence:
-        if word == word:
-            count += 1
-            return count
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+    result = ""
+    for word, count in dictionary.items():
+        result += f"{word}: {count}\n"
+
+    return result
 
 print(count_word())
